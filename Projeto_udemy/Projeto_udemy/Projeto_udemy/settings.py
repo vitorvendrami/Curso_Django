e4e657196d25c8d379c0ac4b2e2b666f3202712b
Vitor_Vendrami <vitor_vendrami@hotmail.com>
2020-04-22 16:16:33 -0300

    C9 - codificando a barra de navegação

--- Projeto_udemy/Projeto_udemy/Projeto_udemy/settings.py
+++ Projeto_udemy/Projeto_udemy/Projeto_udemy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crud'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'Projeto_udemy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'crud.sqlite3'),
     }
 }
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
