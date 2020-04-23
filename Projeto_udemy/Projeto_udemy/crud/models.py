b5dff092ff86b17308a2f85423cfeab811dc5a3b
Vitor_Vendrami <vitor_vendrami@hotmail.com>
2020-04-23 12:55:59 -0300

    C17 inserindo modelo de dados

--- Projeto_udemy/Projeto_udemy/crud/models.py
+++ Projeto_udemy/Projeto_udemy/crud/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+	nome = models.CharField(max_length=100)
+	idade = models.IntegerField()
+	sexo = models.CharField(max_length=1)
+	salario = models.DecimalField(max_digits=7, decimal_places=2)
+
+	def __str__(self):
+		return self.nome
